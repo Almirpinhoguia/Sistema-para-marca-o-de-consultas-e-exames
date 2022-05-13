032d598144db3d1a356217d42f0ec908eddfc6d0
Almirpinhoguia <105524802+Almirpinhoguia@users.noreply.github.com>
2022-05-13 20:24:43 -0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+Print('Olá! Seja bem vindo ao sistema de agendamento do prontuario digital para a marcação de consultas e exames'
+Print('Já possui cadastro em nosso sistema?')
+'''Opção= input('1- Sim\2- Não'))'''
+If Opção ==1:
+    Print('Digite o número do seu cpf e senha para ter acesso aos serviços:')
+Cpf = int(input('Digite o número do seu cpf. (Ex: 10000000000): '))
+Print('Ok! Agora digite a sua senha de acesso a conta:')
+Senha= int(input('Digite a sua senha: '))
+Print('Digite um número para o serviço que você deseja:')
+'''Opção= int(input('Digite 1: Marcação de consultas\2: Marcação de exames'))
+Elif opção ==1:
+     Print('Agora digite o número da especialidade desejada:')
+'''Esp= input(1-Clínica médica\2-Cardiologia\3-Neurologia\4-Ginecologia\5-Dermatologia\6-Infectologia\7-Gastroenterologia\8-Oftalmologia\9-Ortopedia\10-Reumatologia\10- Endocrinologia\11-Otorrinolaringologista')'''
+Print('Escolha o tipo de exame que deseja realizar:')
+'''Exames= str(input('1-Eletrocardiograma\2-Ecocardiograma\3-Eletroencefalograma\4-Mapa 24 horas\5-Holter 24 horas\6-Raio x\7-Exames laboratoriais\8-Ultrassonografia
